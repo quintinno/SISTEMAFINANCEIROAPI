85e88b8cad7a27a69c0b269c53829f6763feb985
Desenvolvimento <repository.desenvolvimento@gmail.com>
2021-07-08 10:34:02 -0300

    SISTEMAFINANCEIRO-07072021234611
    - Funcionalidade: Manter Despesa

---
+++ src/main/resources/dump/drop.tables.sql
@@ -0,0 +1,73 @@
+DROP TABLE public.tb_arquivo CASCADE;
+
+DROP TABLE public.tb_bandeira_cartao_bancario CASCADE;
+
+DROP TABLE public.tb_beneficio CASCADE;
+
+DROP TABLE public.tb_beneficio_valor CASCADE;
+
+DROP TABLE public.tb_cartao_bancario CASCADE;
+
+DROP TABLE public.tb_categoria_cartao_bancario CASCADE;
+
+DROP TABLE public.tb_categoria_despesa CASCADE;
+
+DROP TABLE public.tb_categoria_documento CASCADE;
+
+DROP TABLE public.tb_categoria_receita CASCADE;
+
+DROP TABLE public.tb_chave_pix CASCADE;
+
+DROP TABLE public.tb_cidade CASCADE;
+
+DROP TABLE public.tb_composicao_salario CASCADE;
+
+DROP TABLE public.tb_conta_bancaria CASCADE;
+
+DROP TABLE public.tb_contrato CASCADE;
+
+DROP TABLE public.tb_despesa CASCADE;
+
+DROP TABLE public.tb_documento CASCADE;
+
+DROP TABLE public.tb_email CASCADE;
+
+DROP TABLE public.tb_endereco CASCADE;
+
+DROP TABLE public.tb_estado CASCADE;
+
+DROP TABLE public.tb_fatura_bancaria CASCADE;
+
+DROP TABLE public.tb_forma_pagamento CASCADE;
+
+DROP TABLE public.tb_forma_pagamento_despesa CASCADE;
+
+DROP TABLE public.tb_funcao_cartao_bancario CASCADE;
+
+DROP TABLE public.tb_pais CASCADE;
+
+DROP TABLE public.tb_parcelamento CASCADE;
+
+DROP TABLE public.tb_pessoa CASCADE;
+
+DROP TABLE public.tb_produto_bancario CASCADE;
+
+DROP TABLE public.tb_produto_servico CASCADE;
+
+DROP TABLE public.tb_produto_servico_ocorrencia CASCADE;
+
+DROP TABLE public.tb_receita CASCADE;
+
+DROP TABLE public.tb_tipo_conta_bancaria CASCADE;
+
+DROP TABLE public.tb_tipo_contrato CASCADE;
+
+DROP TABLE public.tb_tipo_endereco CASCADE;
+
+DROP TABLE public.tb_tipo_pessoa CASCADE;
+
+DROP TABLE public.tb_tipo_telefone CASCADE;
+
+DROP TABLE public.tb_tipo_usuario CASCADE;
+
+DROP TABLE public.tb_usuario_sistema CASCADE;
